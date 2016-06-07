0f10587ede623b282076cdcbc7c3e9381f240017
Ilya <cuprum19930@gmail.com>
2016-06-07 20:43:09 +0500

    Changed disappearing of blocks.

--- scr.js
+++ scr.js
@@ -7,10 +7,20 @@ var responsive = document.getElementById('resp');
 var respCSS = document.getElementById('resp-css');
 var adaptCSS = document.getElementById('adapt-css');
 
+function marginRes(){
+  document.getElementsByClassName('passed')[2].style.margin = "0px 20px 0px 0px";
+};
+function RespMarg() {
+  document.getElementsByClassName('responsive')[0].style.margin = "0px 0px 0px 0px";
+};
+function RespCSSMarg() {
+  document.getElementsByClassName('resp-css')[0].style.margin = "0px 0px 0px 0px";
+}
 js.onclick = function(){
+
   mask[0].style.backgroundColor = "rgba(111, 212, 44, 0.37)";
     setTimeout(function() {
-      mask[0].style.display = "none", block[0].style.display = "none", hide();
+      mask[0].style.display = "none", block[0].style.display = "none", hide(), marginRes(), RespMarg();
     }, 1000);
 
   };
@@ -18,20 +28,40 @@ js.onclick = function(){
 jquery.onclick = function() {
   mask[1].style.backgroundColor = "rgba(111, 212, 44, 0.37)";
     setTimeout(function() {
-      mask[1].style.display = "none", block[1].style.display = "none", hide();
+      mask[1].style.display = "none", block[1].style.display = "none", hide(), marginRes()
+      if ((block[0].style.display == "none") && (block[1].style.display == "none")) {
+        console.log('heu');
+        block[2].style.margin = "0px 20px 0px 0px";
+        block[3].style.margin = "0px 20px 0px 0px";
+        block[4].style.margin = "0px 0px 0px 0px";
+      };;
     }, 1000);
+
 };
 basic.onclick = function() {
   mask[2].style.backgroundColor = "rgba(111, 212, 44, 0.37)";
     setTimeout(function() {
-      mask[2].style.display = "none", block[2].style.display = "none", hide();
+      mask[2].style.display = "none", block[2].style.display = "none", hide()
+      if ((block[0].style.display == "none") && (block[1].style.display == "none") && (block[2].style.display == "none")) {
+        console.log('heu');
+        block[2].style.margin = "0px 20px 0px 0px";
+        block[3].style.margin = "0px 20px 0px 0px";
+        block[4].style.margin = "0px 20px 0px 0px";
+      };;
     }, 1000);
 };
 
 resp.onclick = function () {
   mask[3].style.backgroundColor = "rgba(111, 212, 44, 0.37)";
     setTimeout(function() {
-      mask[3].style.display = "none", block[3].style.display = "none", hide();
+      mask[3].style.display = "none", block[3].style.display = "none", hide()
+      if ((block[0].style.display == "none") && (block[1].style.display == "none") && (block[2].style.display == "none") && (block[3].style.display == "none")) {
+        console.log('heu');
+        block[2].style.margin = "0px 20px 0px 0px";
+        block[3].style.margin = "0px 20px 0px 0px";
+        block[4].style.margin = "0px 20px 0px 0px";
+        block[5].style.margin = "0px 20px 0px 0px";
+      };;
     }, 1000);
 };
 respCSS.onclick = function () {
